277c613cd980c274b623b7a7a05cb03a2019f073
LEE BYUNGNAM <bol0615@naver.com>
2019-09-05 15:33:07 +0900

    터렛에서(임시: 대포아님) 마나스톤 발사 반복

---
+++ Switcher/Assets/02.Scripts/MagicCircle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircle : MonoBehaviour
+{
+    // 대포에서 생성할 마나스톤
+    private GameObject manastone;
+
+    // 마나스톤대포
+    public ManastoneFire manaCannon;
+    // 마나스톤이 마법진에 몇개 들어왔는지 카운트
+    private int inCount = 0;
+
+    // 마나스톤 발사 멈춤
+    private bool isStop = false;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 같은 마법진에 닿았는지 확인
+        if(collision.gameObject.tag == gameObject.tag)
+        {
+            inCount++;
+            // 들어온 마나스톤이 하나일경우
+            if (inCount == 1)
+            {
+                isStop = false;
+                // 닿은 마나스톤을 저장
+                manastone = collision.gameObject;
+                StartCoroutine("FireLoop");
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // 마나스톤이 마법진에서 나갈때
+        if (collision.gameObject.tag == gameObject.tag)
+        {
+            inCount--;
+            // 마나스톤 다 빠져나가면 발사 멈춤
+            if (inCount == 0)
+            {
+                isStop = true;
+            }
+        }
+    }
+
+    // 마나스톤 발사 반복
+    public IEnumerator FireLoop()
+    {
+        if (!isStop && manaCannon)
+        {
+            manaCannon.Fire();
+            yield return new WaitForSeconds(8.0f);
+            StartCoroutine("FireLoop");
+        }
+    }
+}
---
+++ Switcher/Assets/02.Scripts/ManastoneFire.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManastoneFire : MonoBehaviour
+{
+    private Rigidbody rig;
+
+    void Start()
+    {
+        rig = gameObject.GetComponent<Rigidbody>();
+        gameObject.SetActive(false);
+    }
+
+    // 마나스톤 활성화 및 발사
+    public void Fire()
+    {
+        gameObject.SetActive(true);
+        rig.AddRelativeForce(Vector3.up * 700.0f);
+        StartCoroutine("ManastoneDestoryDelay");
+    }
+
+    // 마나스톤 파괴 딜레이
+    public IEnumerator ManastoneDestoryDelay()
+    {
+        yield return new WaitForSeconds(7.0f);
+        gameObject.SetActive(false);
+        rig.velocity = Vector3.zero;
+        transform.position = gameObject.transform.parent.position;
+    }
+}
