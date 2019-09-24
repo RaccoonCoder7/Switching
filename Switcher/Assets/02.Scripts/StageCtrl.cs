8b0c96a9e9d07fe4440516093e629d1212ee2038
DK_O <odk1193@gmail.com>
2019-09-24 16:27:27 +0900

    버그픽스2

---
+++ Switcher/Assets/02.Scripts/StageCtrl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCtrl : MonoBehaviour
+{
+    private int nowStage;
+    private GameMgr gameMgr;
+    private AudioSource audio;
+
+    public AudioClip[] BGMClips;
+
+    private void Start()
+    {
+        gameMgr = GameObject.Find("GameMgr").GetComponent<GameMgr>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void CreateStage(int stageNum)
+    {
+        nowStage = stageNum;
+        ShowLoadingScene();
+    }
+
+    private void ShowLoadingScene()
+    {
+        StartCoroutine(gameMgr.FadeIn());
+    }
+
+    private AudioClip GetBGM()
+    {
+        return null;
+    }
+
+    private void CreateMap()
+    {
+
+    }
+
+    private Vector3 GetSpawnPos()
+    {
+        return Vector3.zero;
+    }
+}
