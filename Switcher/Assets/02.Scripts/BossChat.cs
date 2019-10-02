f6175a4e2bf53b0f6d051bc55ff52b1a5a19baeb
smpk32 <smpk32@naver.com>
2019-10-02 15:39:27 +0900

    다시 푸쉬ㅠ 다시하기, 스테이지2 프리팹 수정

--- Switcher/Assets/02.Scripts/BossChat.cs
+++ Switcher/Assets/02.Scripts/BossChat.cs
@@ -7,14 +7,10 @@ using UnityEngine.SceneManagement;
 
 namespace MyDedlegate
 {
-    public delegate void Deleg();
+    public delegate void mDeleg();
 
-    public class Chat : MonoBehaviour
+    public class BossChat : MonoBehaviour
     {
-        private Ray ray;
-        private RaycastHit hit;
-        public LineRenderer laser;
-
         GameMgr gameMgr;
         TextAsset textData;
         StringReader sr;
@@ -23,14 +19,28 @@ namespace MyDedlegate
         string helperTextList;
         int textCount;
         int continueCnt;
+
+        public Text text;
+        public bool helpCheck;
+        public Deleg[] chatEventList = new Deleg[6];
+
+        TextAsset bossTextData;
+        StringReader bossSr;
+        List<string> bossTextList;
+        string bossTextFile;
+        int bossTextCount;
+        public Text bossText;
+        bool bossCheck = false;
+        public GameObject bossCanvas;
+
+
         int paragraphCnt;
         State nowState;
         private StageCtrl sc;
         public TouchMgr touchMgr;
         private TouchMgr.SkillMode prevMode;
-        public Text text;
-        public bool helpCheck;
-        public Deleg[] chatEventList = new Deleg[6];
+
+
 
         AudioSource audio;
 
@@ -45,115 +55,158 @@ namespace MyDedlegate
             gameMgr = FindObjectOfType<GameMgr>();
             audio = GetComponent<AudioSource>();
             sc = FindObjectOfType<StageCtrl>();
+            // touchMgr = FindObjectOfType<TouchMgr>();
+            TextSet("test");
+            BossTextSet();
+            bossCanvas.SetActive(false);
+
         }
 
+        // Update is called once per frame
         void Update()
         {
-            ray = new Ray(laser.transform.position, laser.transform.forward);
-            if (Physics.Raycast(ray, out hit, 16.0f))
-            {
-                float dist = hit.distance;
-                laser.SetPosition(1, new Vector3(0, 0, dist));
-            }
-            laser.enabled = true;
             if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger))
             {
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("RETRY")))
-                {
-                    laser.enabled = false;
-                    StartCoroutine(sc.ResetStage(null));
-                }
-                else
-                {
-                    NextText();
-                }
+                NextText();
             }
+            //if (OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
+            //{
+            //    CallHelper();
+            //}
         }
 
-        // 텍스트를 처음부터 다시 보여주도록 함
         public void ResetText()
         {
             continueCnt = 0;
             textCount = 0;
             paragraphCnt = 0;
-            touchMgr.ChangeMode(TouchMgr.SkillMode.chat);
             NextText();
         }
 
-        // 텍스트를 리소스에서 로드하여 저장함
         public void TextSet(string str)
         {
+            Debug.Log("t1");
             continueCnt = 0;
             textCount = 1;
             textList = new List<string>();
             textData = Resources.Load(str + "Text", typeof(TextAsset)) as TextAsset;
+            Debug.Log("t2");
             sr = new StringReader(textData.text);
             textFile = sr.ReadLine();
             textList.Add(textFile);
+            Debug.Log("t3");
             StartCoroutine(PlayLine(textList[0]));
+            Debug.Log("t4");
             while (textFile != null)
             {
                 textFile = sr.ReadLine();
                 textList.Add(textFile);
             }
+            Debug.Log("t5");
+            // prevMode = touchMgr.mode;
+            // touchMgr.ChangeMode(TouchMgr.SkillMode.chat);
+            Debug.Log("t6");
         }
 
-        // 다음줄을 읽음.
+        public void BossTextSet()
+        {
+            bossCanvas.SetActive(false);
+            bossTextCount = 0;
+            bossTextList = new List<string>();
+            bossTextData = Resources.Load("boss", typeof(TextAsset)) as TextAsset;
+            bossSr = new StringReader(bossTextData.text);
+            bossTextFile = bossSr.ReadLine();
+            bossTextList.Add(bossTextFile);
+            while (bossTextFile != null)
+            {
+                bossTextFile = bossSr.ReadLine();
+                bossTextList.Add(bossTextFile);
+            }
+        }
+
         public void NextText()
         {
             if (nowState.Equals(State.Next))
             {
                 audio.Play();
-
                 //불러온 텍스트중 false가 있으면 아래 실행
                 if (textList[textCount].Equals("false"))
                 {
+                    //textCount++;
                     text.text = "";
                     textCount++;
-                    chatEventList[paragraphCnt]();
+                    Debug.Log("PC: " + paragraphCnt);
+                    //chatEventList[paragraphCnt]();
+                    // paragraphCnt++;
+                    // touchMgr.ChangeMode(prevMode);
                     gameObject.SetActive(false);
                 }
                 //불러온 텍스트중 clear가 있으면 아래 실행
                 else if (textList[textCount].Equals("clear"))
                 {
+                    // gameMgr.Clear();
                     sc.StartCoroutine(sc.ClearStage());
                 }
-                else
+                else if (textList[textCount].Equals("boss"))
+                {
+                    text.text = "";
+                    textCount++;
+                    gameObject.SetActive(false);
+                    bossCanvas.SetActive(true);
+                    StartCoroutine(BossPlayLine(bossTextList[bossTextCount]));
+                    bossTextCount++;
+                    bossCheck = true;
+                }
+                else if (bossTextList[bossTextCount].Equals("bossfalse"))
+                {
+                    bossText.text = "";
+                    bossTextCount++;
+                    bossCanvas.SetActive(false);
+                }
+                else if (bossCheck)
+                {
+                    StartCoroutine(BossPlayLine(bossTextList[bossTextCount]));
+                    bossTextCount++;
+                }
+                else if (!bossCheck)
                 {
                     StartCoroutine(PlayLine(textList[textCount]));
+                    //text.text = textList[textCount];
                     textCount++;
                 }
             }
         }
 
-        // 조력자를 불렀을 때 사용하는 함수
+        //조력자를 불렀을 때 사용하는 함수
         public void CallHelper()
         {
             prevMode = touchMgr.mode;
+            Debug.Log("before");
             touchMgr.ChangeMode(TouchMgr.SkillMode.chat);
+            Debug.Log("after");
             StartCoroutine(PlayLine(helperTextList));
             textCount = continueCnt;
             helpCheck = true;
         }
-
-        // 조력자를 없앰
         public void FadeHelper()
         {
             touchMgr.ChangeMode(prevMode);
             textCount = continueCnt;
             gameObject.SetActive(false);
             helpCheck = false;
+
         }
 
         //다음 대화를 진행할 때 부르는 메소드
         public void NextChat()
         {
             continueCnt = textCount;
+            Debug.Log("NextChat");
+            // gameObject.SetActive(true);
             StartCoroutine(PlayLine(textList[textCount]));
             paragraphCnt++;
             textCount++;
         }
-
         IEnumerator PlayLine(string setText)
         {
             nowState = State.Playing;
@@ -161,6 +214,17 @@ namespace MyDedlegate
             {
                 yield return new WaitForSeconds(0.02f);
                 text.text = setText.Substring(0, i);
+            }
+            yield return new WaitForSeconds(0.5f);
+            nowState = State.Next;
+        }
+        IEnumerator BossPlayLine(string setText)
+        {
+            nowState = State.Playing;
+            for (int i = 0; i < setText.Length + 1; i += 1)
+            {
+                yield return new WaitForSeconds(0.02f);
+                bossText.text = setText.Substring(0, i);
             }
             yield return new WaitForSeconds(0.5f);
             nowState = State.Next;
