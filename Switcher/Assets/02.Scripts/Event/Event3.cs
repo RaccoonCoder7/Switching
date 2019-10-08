2438a3cb81870dc0161df928ab666fa0cae01e7a
LEE BYUNGNAM <bol0615@naver.com>
2019-10-08 16:55:20 +0900

    빔콜라이더 레이어 추가, 마나스톤이랑 충돌 안되게 수정, 스테이지 3 이벤트

---
+++ Switcher/Assets/02.Scripts/Event/Event3.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyDedlegate;
+
+public class Event3 : EventMgr
+{
+    private GameObject barrier;
+    private Timer timer;
+    private TouchMgr touchMgr;
+    bool modeCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        GameObject player = GameObject.Find("Player");
+        barrier = player.transform.Find("BeamupCylinderGreen").gameObject;
+        timer = FindObjectOfType<Timer>();
+        touchMgr = player.GetComponent<TouchMgr>();
+        touchMgr.mode.Equals(TouchMgr.SkillMode.chat);
+
+        EventList[0] = new Deleg(EV1);
+        EventList[1] = new Deleg(EV2);
+        EventList[2] = new Deleg(EV3);
+    }
+    private void Update()
+    {
+        if (modeCheck && touchMgr.mode.Equals(TouchMgr.SkillMode.pull))
+        {
+            touchMgr.ChangeMode(TouchMgr.SkillMode.chat);
+            CallChat();
+            modeCheck = false;
+        }
+    }
+
+    private void EV1()
+    {
+        StartCoroutine("FadeInOutBarrier");
+    }
+    private void EV2()
+    {
+        touchMgr.ChangeMode(TouchMgr.SkillMode.switching);
+        modeCheck = true;
+    }
+    private void EV3()
+    {
+        touchMgr.ChangeMode(TouchMgr.SkillMode.pull);
+        timer.StartTime();
+    }
+
+    private IEnumerator FadeInOutBarrier()
+    {
+        barrier.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        barrier.SetActive(false);
+        CallChat();
+    }
+}
