e4d94f8208fbdcdf0a789522bc8683ef6c0ee7e5
LEE BYUNGNAM <bol0615@naver.com>
2019-10-08 17:34:37 +0900

    스테이지 시간, 스테이지4 이벤트

--- Switcher/Assets/02.Scripts/Event/Event4.cs
+++ Switcher/Assets/02.Scripts/Event/Event4.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MyDedlegate;
 
-public class Event4 : MonoBehaviour
+public class Event4 : EventMgr
 {
+    bool modeCheck;
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();
+
+        EventList[0] = new Deleg(EV1);
+        EventList[1] = new Deleg(EV2);
+        EventList[2] = new Deleg(EV3);
+    }
+    private void Update()
+    {
+        if (modeCheck && touchMgr.mode.Equals(TouchMgr.SkillMode.push))
+        {
+            touchMgr.ChangeMode(TouchMgr.SkillMode.chat);
+            CallChat();
+            modeCheck = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EV1()
     {
-        
+        StartCoroutine("FadeInOutBarrier");
+    }
+    private void EV2()
+    {
+        touchMgr.ChangeMode(TouchMgr.SkillMode.switching);
+        modeCheck = true;
+    }
+    private void EV3()
+    {
+        touchMgr.ChangeMode(TouchMgr.SkillMode.push);
+        touchMgr.canFire = true;
+        timer.StartTime();
+    }
+
+    private IEnumerator FadeInOutBarrier()
+    {
+        barrier.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        barrier.SetActive(false);
+        CallChat();
     }
 }
