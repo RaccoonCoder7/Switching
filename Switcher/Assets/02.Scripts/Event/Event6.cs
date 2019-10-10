88bb2476117eb3528bc8b409bec1e86706d59006
smpk32 <smpk32@naver.com>
2019-10-10 16:28:15 +0900

    보스채팅 될듯ㅠ

--- Switcher/Assets/02.Scripts/Event/Event6.cs
+++ Switcher/Assets/02.Scripts/Event/Event6.cs
@@ -7,34 +7,40 @@ public class Event6 : EventMgr
 {
     bool modeCheck;
     protected BossChat bossChat;
+    public GameObject bossChatCanvas;
     // Start is called before the first frame update
     void Start()
     {
         base.Start();
-        bossChat = FindObjectOfType<BossChat>();
-
+        bossChat = bossChatCanvas.GetComponent<BossChat>();
+        bossChat.bossTextCount = 1;
+        bossChat.paragraphCnt = 0;
+        bossChatCanvas.SetActive(false);
         EventList[0] = new Deleg(EV1);
         EventList[1] = new Deleg(EV2);
         EventList[2] = new Deleg(EV3);
-    }
-    private void Update()
-    {
-
+        EventList[3] = new Deleg(EV4);
+        EventList[4] = new Deleg(EV5);
     }
 
     private void EV1()
     {
-        CallChat();
+        bossChatCanvas.SetActive(true);
+        bossChat.BossTextSet();
     }
     private void EV2()
     {
-        CallBossChat();
+        CallChat();
     }
     private void EV3()
     {
-        CallChat();
+        CallBossChat();
     }
     private void EV4()
+    {
+        CallChat();
+    }
+    private void EV5()
     {
         touchMgr.canFire = true;
         timer.StartTime();
@@ -48,7 +54,7 @@ public class Event6 : EventMgr
     private IEnumerator WaitAndCallBossChat()
     {
         yield return new WaitForSeconds(0.1f);
-        bossChat.gameObject.SetActive(true);
+        bossChatCanvas.SetActive(true);
         bossChat.NextChat();
     }
 }
